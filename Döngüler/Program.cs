d40e0bda48df38c449ae59f15bf6ee8e57f00b97
Abdülsamet YILMAZ <mr.asmtylmz@gmail.com>
2022-11-05 19:33:25 +0300

    Proje dosyası ekle.

---
+++ Döngüler/Program.cs
@@ -0,0 +1,19 @@
+﻿int faktoriyel = 1;
+Console.Write("Hesaplamak İstediğiniz Sayıyı Giriniz : ");
+int sayi = Convert.ToInt16(Console.ReadLine());
+int i;
+for (i = 1; i <= sayi; i++)
+{
+    faktoriyel *= i;
+}
+Console.WriteLine("Girilen değer :"+sayi +"!");
+Console.WriteLine("Sonuç : " + faktoriyel);
+
+
+
+
+
+
+
+
+
---
+++ algoritmaEb/Program.cs
@@ -0,0 +1,12 @@
+﻿int x;
+int y;
+int z;
+
+Console.WriteLine("1. SAYI : ");
+x = Convert.ToInt16(Console.Read());
+Console.WriteLine("2. SAYI : ");
+y = Convert.ToInt16(Console.Read());
+Console.WriteLine("3. SAYI : ");
+z = Convert.ToInt16(Console.Read());
+
+Console.WriteLine(x + y + z);
